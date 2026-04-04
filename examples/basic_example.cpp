384c29ca768a10209539fe185c23a51856699df0
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-04-04 21:48:59 +0600

    add jpeg, pngm jpg in git ignore

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -5,15 +5,29 @@
 #include "shiftstitch/shiftstitch.hpp"
 #include "shiftstitch/sift.hpp"
 
+using namespace shiftstitch;
+
 int main() {
-	std::vector<std::string> images_path = {
+	std::vector<std::string> room = {
 	    "input/room/room01.jpeg",
 	    "input/room/room02.jpeg",
 	};
 
-	shiftstitch::ShiftStitcher shiftstitch(images_path);
+	std::vector<std::string> back = {
+	    "input/back/back_01.jpeg",
+	    "input/back/back_02.jpeg",
+	    "input/back/back_03.jpeg",
+	};
 
-	shiftstitch::SIFT siftAlgorithm;
+	std::vector<std::string> front = {
+	    "input/front/front_01.jpeg",
+	    "input/front/front_02.jpeg",
+	    "input/front/front_03.jpeg",
+	};
+
+	ShiftStitcher shiftstitch(front);
+
+	SIFT siftAlgorithm;
 	shiftstitch.createPanorama(siftAlgorithm);
 
 	shiftstitch.savePanorama("output/panorama.jpg");
