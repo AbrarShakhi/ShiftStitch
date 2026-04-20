bd88901870f3f9d1daa5835ca3dcbc5b11efe51b
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-04-20 20:06:50 +0600

    renamed istitcher

--- include/shiftstitch/istitcher.hpp
+++ include/shiftstitch/istitcher.hpp
@@ -3,11 +3,14 @@
 #include <opencv2/core/mat.hpp>
 #include <vector>
 
+#include "result.hpp"
+
 namespace shiftstitch {
 
-class ISticher {
+class IStitcher {
 public:
-	virtual cv::Mat stitch(std::vector<cv::Mat>& images) = 0;
+	virtual Result<cv::Mat> stitch(std::vector<cv::Mat>& images) = 0;
+	virtual ~IStitcher() = default;
 };
 
 }  // namespace shiftstitch
