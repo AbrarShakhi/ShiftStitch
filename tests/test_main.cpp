e4f53e2ae5da24f2088771117377667bbf489a61
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-04-20 20:49:43 +0600

    fix testing and cmakelists

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,6 +6,6 @@ int main() {
 	// Minimal test
 	std::vector<std::string> images_path = {};
 
-	shiftstitch::ShiftStitcher shiftstitch(images_path);
+	auto stitcherResult = shiftstitch::ShiftStitcher::create(images_path);
 	return 0;
 }
