d0b740dc7c9f704e5949a58bab834ae6ac08a04b
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-04-22 14:10:27 +0600

    output file name can be inserted threw argument

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -9,13 +9,19 @@
 using namespace shiftstitch;
 
 int main(int argc, char* argv[]) {
-	if (argc < 3) {
-		std::cerr << "Usage: " << argv[0] << " <image1> <image2> image3 ...\n";
+	if (argc < 4) {
+		std::cerr << "Usage: " << argv[0] << " <image1> <image2> [image3 ...] <output>\n";
 		return EXIT_FAILURE;
 	}
+
 	std::vector<std::string> image_list;
-	for (int i = 1; i < argc; ++i)
+
+	// All except last argument are input images
+	for (int i = 1; i < argc - 1; ++i)
 		image_list.push_back(argv[i]);
+
+	// Last argument is output file
+	std::string output_file = argv[argc - 1];
 
 	auto stitcher_result = ShiftStitcher::create(image_list);
 	if (stitcher_result.isErr()) {
@@ -32,12 +38,12 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	auto save_result = stitcher.savePanorama("output.jpg");
+	auto save_result = stitcher.savePanorama(output_file);
 	if (save_result.isErr()) {
 		std::cerr << save_result.error().toString() << '\n';
 		return EXIT_FAILURE;
 	}
 
-	std::cout << "Panorama saved successfully.\n";
+	std::cout << "Panorama saved successfully to " << output_file << ".\n";
 	return EXIT_SUCCESS;
 }
